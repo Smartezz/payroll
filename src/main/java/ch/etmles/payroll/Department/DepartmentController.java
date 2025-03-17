3d07d3ea2f23a53881cbbb491ecda8e44f8dfa1d
Smartezz <pm17nrr@eduvaud.ch>
2025-03-17 11:19:35 +0100

    Ajout de la couche service pour Employee

--- src/main/java/ch/etmles/payroll/Department/DepartmentController.java
+++ src/main/java/ch/etmles/payroll/Department/DepartmentController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/departments")
+@RequestMapping("/api/v1/departments")
 public class DepartmentController {
 
     private final DepartmentRepository repository;
